5b7a6ccecabe6e2582aa067aae4a1d6a00ad1435
Farouk <faroukkisuule55@gmail.com>
2021-05-13 21:37:31 -0400

    Mongo DB successfully connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect Database
+connectDB();
 
 app.get("/", (req,res)=>{
     res.send("<h1>API running well</h1>")
