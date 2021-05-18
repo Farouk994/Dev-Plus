0b801d01c64f9307292878a918370e516dbf7957
Farouk <faroukkisuule55@gmail.com>
2021-05-18 15:23:49 -0400

    Added User Profile Route and updated Profile models

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
 
 // @route GET api/users
-router.get("/",(req,res)=>{
-    res.send('Profile Route')
+router.get("/", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.id }).populate([
+      "user",
+      "avatar",
+    ]);
+    if(!profile){
+        return res.status(400).json({ msg : "There is no profile for this user"})
+    }
+  } catch (err) {
+      res.status(400).json("Server Error")
+  }
 });
 
 module.exports = router;
