419b9c2f9094b05b8f33f13a965e18bdeede8cb4
Farouk <faroukkisuule55@gmail.com>
2021-06-05 09:44:56 -0400

    Added delete comment functionality route

--- routes/api/post.js
+++ routes/api/post.js
@@ -177,15 +177,15 @@ router.post(
     }
     try {
       const user = await User.findById(req.user.id).select("-password");
-      const post = await Posts.findById(req.params.id)
+      const post = await Posts.findById(req.params.id);
 
-      const newComment ={
+      const newComment = {
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id,
       };
- 
+
       post.comments.unshift(newComment);
       await post.save();
       res.json(post.comments);
@@ -197,6 +197,45 @@ router.post(
   }
 );
 
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Comment on Post
+// @access Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    // Get the Post
+    const post = await Posts.findById(req.params.id);
+    //Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
 
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    // Check User
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User Not Authorised" });
+    }
+    // GET remove index
+    // const removeIndex = post.comments
+    //   .map((comment) => comment.user.toString())
+    //   .indexOf(req.user.id);
+    // post.comments.splice(removeIndex, 1);
+
+    // Return new Array of comments that are not equal to comment_id 
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
