0ece4e9ddb717a776161049b79ae48d15726ad9c
Farouk <faroukkisuule55@gmail.com>
2021-06-07 17:45:33 -0400

    Added register form using React

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,25 +6,19 @@ const Navbar = () => {
     <div>
       <nav className='navbar bg-dark'>
         <h1>
-          <Link to ='/'>
+          <Link to='/'>
             <i className='fas fa-code'></i> Dev+
           </Link>
         </h1>
         <ul>
           <li>
-            <Link to ='profiles.html'>
-              Developers
-            </Link>
+            <Link to='profiles.html'>Developers</Link>
           </li>
           <li>
-            <Link to ='/register'>
-              Register
-            </Link>
+            <Link to='/register'>Register</Link>
           </li>
           <li>
-            <Link to ='/login'>
-              Login
-            </Link>
+            <Link to='/login'>Login</Link>
           </li>
         </ul>
       </nav>
