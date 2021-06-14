8924afbe475886adff5ea049c907f805bce92607
Farouk <faroukkisuule55@gmail.com>
2021-06-14 07:44:32 -0400

    Added add-experience page on the user profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import setAuthToken from "./utils/setAuthToken";
 import PrivateRoute from "./components/Routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
+import AddExperience from "./components/profile-form/AddExperience"
 
 
 import "./App.css";
@@ -41,6 +42,7 @@ const App = () => {
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
             </Switch>
           </section>
         </Fragment>
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,11 +6,18 @@ export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
-export const LOGOUT = "LOGOUT;"
-export const GET_PROFILE = "GET_PROFILE;"
-export const PROFILE_ERROR = "PROFILE_ERROR;"
-export const CLEAR_PROFILE = "CLEAR_PROFILE;"
+export const LOGOUT = "LOGOUT";
+export const GET_PROFILE = "GET_PROFILE";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
  
 // 1. First define types in reducer types file from actions/types
 // 2. Then send it to the same directory (types/auth) to make makes requests to backend
 // 3. After that we send to the reducers to handle the response 
+
+
+// Creating or Posting
+// 1. Types
+// 2. Actions
+// 3. Reducers 
